0fae8067f894287806027954553c6cc96b218157
ramson <ramson@10.1.10.209>
2019-07-04 19:59:16 -0400

    Add Getmapping for add employee in the controller and setting up a html page for add new Employee

--- Thymeleaf-spring-boot.crud/src/main/java/com/ramson/springboot/controller/EmployeeController.java
+++ Thymeleaf-spring-boot.crud/src/main/java/com/ramson/springboot/controller/EmployeeController.java
@@ -37,6 +37,21 @@ public class EmployeeController {
 		//add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 		
-		return "list-employees";
+		return "employees/list-employees";
 	}
+	
+	
+	//add mapping for "/add employee
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		//create a model attribute to bind form data
+		Employee theEmployee = new Employee();
+		
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
+	}
+	
+	
 }
