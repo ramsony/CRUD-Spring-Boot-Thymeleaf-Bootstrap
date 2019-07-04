f6b01023a526e44338cd716ebad630bce3c0255b
ramson <ramson@10.1.10.209>
2019-07-04 18:07:51 -0400

    Adding connection to the database,and setting up mapping for employee list on the controller

--- Thymeleaf-spring-boot.crud/src/main/java/com/ramson/springboot/controller/EmployeeController.java
+++ Thymeleaf-spring-boot.crud/src/main/java/com/ramson/springboot/controller/EmployeeController.java
@@ -1,48 +1,39 @@
 package com.ramson.springboot.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ramson.springboot.entity.Employee;
+import com.ramson.springboot.service.EmployeeService;
 
 
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
-
-	//load employee data
-	private List<Employee>theEmployees;
 	
-	@PostConstruct
-	private void loadData() {
+	private EmployeeService employeeService;
+	
+	//constructor injections
+	@Autowired
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
 		
-		//create employees
-		Employee emp1 = new Employee(1, "Ramson", "Mdee", "ramson@codx.com");
-		Employee emp2 = new Employee(2, "Feruz", "Yasin", "feruzi@codx.com");
-		Employee emp3 = new Employee(3, "Abdul", "Aziz", "abdul@codx.com");
-		Employee emp4 = new Employee(4, "Faith", "Owen", "faith@codx.com");
-		
-		//create the list
-		theEmployees = new ArrayList<Employee>();
-		
-		// add to the list
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
-		theEmployees.add(emp4);
 	}
+
 	
 	//add mapping for "/list"
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
+		
+		//get all the employees from the database
+		List<Employee> theEmployees = employeeService.findAll();
+		
 		//add to the spring model
 		theModel.addAttribute("employees", theEmployees);
 		
